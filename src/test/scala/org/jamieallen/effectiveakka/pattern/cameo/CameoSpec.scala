34f542e00592352f411dbad7bb10208f51da863f
Jamie Allen <shinolajla@yahoo.com>
2013-05-19 15:55:18 -0700

    Fixed within blocks

--- src/test/scala/org/jamieallen/effectiveakka/pattern/cameo/CameoSpec.scala
+++ src/test/scala/org/jamieallen/effectiveakka/pattern/cameo/CameoSpec.scala
@@ -21,12 +21,12 @@ class CameoSpec extends TestKit(ActorSystem("CameoTestAS")) with ImplicitSender 
       val moneyMarketAccountsProxy = system.actorOf(Props[MoneyMarketAccountsProxyStub], "cameo-success-money-markets")
       val accountBalanceRetriever = system.actorOf(Props(new AccountBalanceRetriever(savingsAccountsProxy, checkingAccountsProxy, moneyMarketAccountsProxy)), "cameo-retriever1")
 
-      within(0 milliseconds, 300 milliseconds) {
+      within(300 milliseconds) {
         probe1.send(accountBalanceRetriever, GetCustomerAccountBalances(1L))
         val result = probe1.expectMsgType[AccountBalances]
         result must equal(AccountBalances(Some(List((3, 15000))), Some(List((1, 150000), (2, 29000))), Some(List())))
       }
-      within(0 milliseconds, 300 milliseconds) {
+      within(300 milliseconds) {
         probe2.send(accountBalanceRetriever, GetCustomerAccountBalances(2L))
         val result = probe2.expectMsgType[AccountBalances]
         result must equal(AccountBalances(Some(List((6, 640000), (7, 1125000), (8, 40000))), Some(List((5, 80000))), Some(List((9, 640000), (10, 1125000), (11, 40000)))))
--- src/test/scala/org/jamieallen/effectiveakka/pattern/extra/ExtraFinalSpec.scala
+++ src/test/scala/org/jamieallen/effectiveakka/pattern/extra/ExtraFinalSpec.scala
@@ -9,7 +9,6 @@ import org.jamieallen.effectiveakka.common._
 import org.jamieallen.effectiveakka.pattern.extra.AccountBalanceRetrieverFinal._
 
 class ExtraFinalSpec extends TestKit(ActorSystem("ExtraTestAS")) with ImplicitSender with WordSpecLike with MustMatchers {
-
   "An AccountBalanceRetriever" should {
     "return a list of account balances" in {
       val probe2 = TestProbe()
@@ -19,12 +18,12 @@ class ExtraFinalSpec extends TestKit(ActorSystem("ExtraTestAS")) with ImplicitSe
       val moneyMarketAccountsProxy = system.actorOf(Props[MoneyMarketAccountsProxyStub], "extra-success-money-markets")
       val accountBalanceRetriever = system.actorOf(Props(new AccountBalanceRetrieverFinal(savingsAccountsProxy, checkingAccountsProxy, moneyMarketAccountsProxy)), "extra-retriever")
 
-      within(0 milliseconds, 300 milliseconds) {
+      within(300 milliseconds) {
         probe1.send(accountBalanceRetriever, GetCustomerAccountBalances(1L))
         val result = probe1.expectMsgType[AccountBalances]
         result must equal(AccountBalances(Some(List((3, 15000))), Some(List((1, 150000), (2, 29000))), Some(List())))
       }
-      within(0 milliseconds, 300 milliseconds) {
+      within(300 milliseconds) {
         probe2.send(accountBalanceRetriever, GetCustomerAccountBalances(2L))
         val result = probe2.expectMsgType[AccountBalances]
         result must equal(AccountBalances(Some(List((6, 640000), (7, 1125000), (8, 40000))), Some(List((5, 80000))), Some(List((9, 640000), (10, 1125000), (11, 40000)))))
